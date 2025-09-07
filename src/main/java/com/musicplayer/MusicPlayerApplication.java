27ab0de6f94f0e3bf4eaa60369dfdceb6334c37e
nicseamas <study9102016@gmail.com>
2025-09-07 15:24:47 +0100

    Added database dump file and update READ.ME

--- src/main/java/com/musicplayer/MusicPlayerApplication.java
+++ src/main/java/com/musicplayer/MusicPlayerApplication.java
@@ -2,8 +2,7 @@ package com.musicplayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class MusicPlayerApplication {
